f32a6599fde3221e092fc6d4e1487b6f29c35ae0
Eclair <eclairwu075@gmail.com>
2019-05-27 15:01:19 +0800

    triangle and !!!!

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -6,7 +6,7 @@ public class Test {
 
     public static void main (String args[]){
 
-        System.out.println("triangle!!!!");
+        System.out.println("triangle!!!!!");
         for (int i = 0; i < 5; i++) {
             for (int y = 0; y <= i; y++) {
                 System.out.print("*");
