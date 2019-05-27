42dbf6e5a5a5ce9a0675c57fc484c0cbdbbcbaf0
Eclair <eclairwu075@gmail.com>
2019-05-27 14:38:09 +0800

    Added project DeveloperResume

---
+++ src/main/java/Test.java
@@ -0,0 +1,18 @@
+/**
+ * Created by Eclair
+ * on 2019/5/10.
+ */
+public class Test {
+
+    public static void main (String args[]){
+
+        System.out.println("triangle!!");
+        for (int i = 0; i < 5; i++) {
+            for (int y = 0; y <= i; y++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+
+}
