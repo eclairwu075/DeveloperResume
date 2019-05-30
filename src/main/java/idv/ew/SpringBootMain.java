f766e4fca3ef722f15e9a71ec61d63b49554e52e
Eclair <eclairwu075@gmail.com>
2019-05-30 22:04:14 +0800

    Spring Boot settings 1. 在pom檔加入SpringBoot的相關dependencies 2. 新增SpringBootMain起始點，及其內容 3. 新增SpringBoot的Server 5. 在pom檔新增thymeleaf的dependency 6. 新增application.properties，及其內容 7. 新增home.html測試

---
+++ src/main/java/idv/ew/SpringBootMain.java
@@ -0,0 +1,15 @@
+package idv.ew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Eclair
+ * on 2019/5/30.
+ */
+@SpringBootApplication
+public class SpringBootMain {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMain.class, args);
+    }
+}
---
+++ src/main/java/idv/ew/site/controller/ViewController.java
@@ -0,0 +1,18 @@
+package idv.ew.site.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Created by Eclair
+ * on 2019/5/30.
+ */
+@Controller
+public class ViewController {
+
+    @GetMapping(value = {"/abc"})
+    public String home(){
+        return "home";
+    }
+
+}
